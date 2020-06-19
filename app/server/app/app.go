6a610047b7aa81c2e9fb0af400d47d9cedf6cd1a
ciazhar <ciazhar.id@gmail.com>
2020-06-19 11:29:03 +0700

    add pg

--- app/server/app/app.go
+++ app/server/app/app.go
@@ -1,15 +1,18 @@
 package app
 
 import (
+	pg2 "github.com/ciazhar/golang-grpc/common/db/pg"
 	"github.com/ciazhar/golang-grpc/common/env"
 	"github.com/ciazhar/golang-grpc/common/logger"
 	"github.com/ciazhar/golang-grpc/common/validator"
 	"github.com/gin-gonic/gin"
+	"github.com/go-pg/pg/v9"
 	"os"
 )
 
 type Application struct {
-	Env *env.Environtment
+	Env      *env.Environtment
+	Postgres *pg.DB
 }
 
 func SetupApp() (*Application, error) {
@@ -28,21 +31,42 @@ func SetupApp() (*Application, error) {
 	//logger
 	logger.InitLogger()
 
+	//postgres
+	pgConn := pg2.InitPG(environment)
+
 	//validator
 	validator.Init()
 
 	return &Application{
-		Env: environment,
+		Env:      environment,
+		Postgres: pgConn,
 	}, nil
 }
 
 func SetupAppWithPath(path string) (*Application, error) {
+
+	//env
 	environment := env.InitPath(path)
+
+	//set default timezone
+	if err := os.Setenv("TZ", "Asia/Jakarta"); err != nil {
+		panic(err.Error())
+	}
+
+	//profile
+	gin.SetMode(environment.Get("profile"))
+
+	//logger
 	logger.InitLogger()
 
+	//postgres
+	pgConn := pg2.InitPG(environment)
+
+	//validator
 	validator.Init()
 
 	return &Application{
-		Env: environment,
+		Env:      environment,
+		Postgres: pgConn,
 	}, nil
 }
